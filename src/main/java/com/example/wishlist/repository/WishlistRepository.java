49cc96f60084dd251bedb0dec95cdb33880745c1
MU-1995 <jynke58@hotmail.com>
2025-10-23 16:15:45 +0200

    tilføjet showwishlist og findwishlistbyid

--- src/main/java/com/example/wishlist/repository/WishlistRepository.java
+++ src/main/java/com/example/wishlist/repository/WishlistRepository.java
@@ -1,16 +1,19 @@
 package com.example.wishlist.repository;
 
 import com.example.wishlist.model.Wish;
+import com.example.wishlist.model.Wishlist;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class WishlistRepository {
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<Wish> wishRowMapper = (rs, rowNum) -> {
         Wish wish = new Wish();
-        wish.setName(rs.getString("name"));
+        wish.setName(rs.getString("wishName"));
         wish.setDescription(rs.getString("description"));
         wish.setLink(rs.getString("link"));
         wish.setPrice(rs.getInt("price"));
@@ -22,8 +25,29 @@ public class WishlistRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void showWishList() {
+    public List<Wish> showWishList() {
 
+        String sql = """
+        SELECT
+        w.wishName,
+        w.description,
+        w.link,
+        w.price,
+        w.isReserved
+        FROM wish w
+        """;
+
+        return jdbcTemplate.query(sql,wishRowMapper);
     }
 
+    public Wishlist findWishlistByID(int wishlistID) {
+
+        String sql = """
+                SELECT
+                wl.wishlistID
+                FROM wishlist wl
+                """;
+
+        return jdbcTemplate.query(sql,);
+    }
 }
